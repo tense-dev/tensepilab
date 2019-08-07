b06f73439ac531657656123318d2f8fc2eca341b
tense <tense@aaa.com>
2019-08-07 13:52:27 +0700

    design ui home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,8 +11,45 @@ class _HomeState extends State<Home> {
   // Explicit
   double myWidth = 120.0;
   double myH1 = 36.0;
+  double myFontButton = 18.0;
+  Color myColorButton = Colors.yellow[800];
 
   // Medtod
+  Widget signUpButton() {
+    return Container(
+      width: 220.0,
+      child: OutlineButton(
+        borderSide: BorderSide(color: myColorButton),
+        child: Text(
+          'Sign Up',
+          style: TextStyle(color: myColorButton),
+        ),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+        color: myColorButton,
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget signInButton() {
+    return Container(
+      width: 220.0,
+      child: RaisedButton(
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+        color: myColorButton,
+        child: Text(
+          'Sign In',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
   Widget showLogo() {
     return Container(
       alignment: Alignment.center,
@@ -38,9 +75,19 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[showLogo(), showAppName()],
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: RadialGradient(
+                colors: [Colors.white, Colors.blueGrey], radius: 1.2)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            showLogo(),
+            showAppName(),
+            signInButton(),
+            signUpButton(),
+          ],
+        ),
       ),
     );
   }
